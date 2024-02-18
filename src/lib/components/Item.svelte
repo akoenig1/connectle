15798382e2ac6515ad2d6281e374b95e419f69c3
Alex Koenig <alex.koenig4@gmail.com>
2024-02-18 12:07:30 -0500

    feat: add Item component

---
+++ src/lib/components/Item.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let data;
+  export let onSelect;
+
+  const handleClick = () => {
+    onSelect();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onSelect();
+    }
+  };
+</script>
+
+<div 
+  class={"item" + (data.selected ? " selected" : "") + (data.mistake ? " invalid-shake" : "")}
+  role="button"
+  tabindex="0"
+  on:click={handleClick} 
+  on:keydown={handleKeyDown}
+>
+  {data.title}
+</div>
