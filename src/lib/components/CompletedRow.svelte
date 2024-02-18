35d359cd56bb7bcdeda24e8251e15a5cb5bf6157
Alex Koenig <alex.koenig4@gmail.com>
2024-02-18 12:08:00 -0500

    feat: add CompletedRow component

---
+++ src/lib/components/CompletedRow.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let data;
+</script>
+
+<div class={"row-complete-wrapper " + "level-" + data.level}>
+  <div class="complete-header">
+      <h3>{data.title}</h3>
+  </div>
+  <p>{data.members.join(", ")}</p>
+</div>
