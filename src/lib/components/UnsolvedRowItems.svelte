2d39182d8cae0b7efc1c9ea227f869d77eedebc9
Alex Koenig <alex.koenig4@gmail.com>
2024-02-18 12:07:49 -0500

    feat: add UnsolvedRowItems component

---
+++ src/lib/components/UnsolvedRowItems.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Item from "./Item.svelte";
+  
+  export let items;
+  export let itemSelected;
+</script>
+
+<div class="row-wrapper">
+  {#each items as item}
+    <Item data={item} onSelect={() => itemSelected(item.id)} />
+  {/each}
+</div>
