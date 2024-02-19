915e821e030afc1c3305e96b8a8704e9159a6d34
Alex Koenig <alex.koenig4@gmail.com>
2024-02-18 20:59:05 -0500

    test: make fixture data more distinguishable

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -13,17 +13,17 @@ export const load = async ({ params }) => {
       {
         title: "Category 2",
         level: 1,
-        items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+        items: ["Item A", "Item B", "Item C", "Item D"],
       },
       {
         title: "Category 3",
         level: 2,
-        items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+        items: ["Item I", "Item II", "Item III", "Item IV"],
       },
       {
         title: "Category 4",
         level: 3,
-        items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+        items: ["One", "Two", "Three", "Four"],
       },
     ],
     timeCreated: new Date(),
