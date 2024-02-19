ee6fbb288f00e981a16695ee20ee0502812f4a90
Alex Koenig <alex.koenig4@gmail.com>
2024-02-18 20:53:21 -0500

    feat: style Items

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -22,3 +22,43 @@
 >
   {data.title}
 </div>
+
+<style lang="scss">
+  $item-grey: #efefe6;
+  $selected-color: #5a594e;
+  
+  .item {
+    width: 150px;
+    height: 80px;
+    margin: 4px;
+    font-size: 16px;
+    background-color: $item-grey;
+    color: black;
+    outline: none;
+    text-align: center;
+    font-family: "Franklin Bold", sans-serif;
+    font-weight: 600;
+    text-transform: uppercase;
+    border-radius: 6px;
+    z-index: 0;
+    // position: absolute;
+    transition: opacity 0.4s ease;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    cursor: pointer;
+    overflow: hidden;
+    word-break: break-word;
+
+    &.selected {
+      color: white;
+      background: $selected-color;
+    }
+  }
+
+.invalid-shake {
+  animation: shake ease-in 0.2s 1.5;
+}
+</style>
