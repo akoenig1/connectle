db74e34f650dcaf8e0ab645f6caeaae4daf5bf06
Alex Koenig <alex.koenig4@gmail.com>
2024-02-19 20:52:44 -0500

    feat: implement [slug] api route and move fixture there

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -1,33 +1,10 @@
-export const load = async ({ params }) => {
-  // const data = await getDataFromSlug(slug);
-  const slug = params.slug;
-  
-  const result = {
-    title: "Test Game",
-    categories: [
-      {
-        title: "Category 1",
-        level: 0,
-        items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-      },
-      {
-        title: "Category 2",
-        level: 1,
-        items: ["Item A", "Item B", "Item C", "Item D"],
-      },
-      {
-        title: "Category 3",
-        level: 2,
-        items: ["Item I", "Item II", "Item III", "Item IV"],
-      },
-      {
-        title: "Category 4",
-        level: 3,
-        items: ["One", "Two", "Three", "Four"],
-      },
-    ],
-    timeCreated: new Date(),
-  };
+export const load = async (event) => {
+  const slug = event.params.slug;
+  const data = await event.fetch(`/api/${slug}`).then(r => {
+    return r.json()}
+  );
+
+  const result = data.result;
 
   return {
     result,
---
+++ src/routes/api/[slug]/+server.js
@@ -0,0 +1,37 @@
+export async function GET() {
+  // const ref = doc(database, "connections", slug).withConverter(dataConverter);
+  // const document = await getDoc(ref);
+  // const data = document.data();
+  const data = {
+    title: "Test Game",
+    categories: [
+      {
+        title: "Category 1",
+        level: 0,
+        items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+      },
+      {
+        title: "Category 2",
+        level: 1,
+        items: ["Item A", "Item B", "Item C", "Item D"],
+      },
+      {
+        title: "Category 3",
+        level: 2,
+        items: ["Item I", "Item II", "Item III", "Item IV"],
+      },
+      {
+        title: "Category 4",
+        level: 3,
+        items: ["One", "Two", "Three", "Four"],
+      },
+    ],
+    timeCreated: new Date(),
+  }
+
+  if (data) {
+    return new Response(JSON.stringify({ result: data, error: "" }), { status: 200 });
+  } else {
+    return new Response(JSON.stringify({ result: null, error: "Error." }), { status: 500 });
+  }
+}
